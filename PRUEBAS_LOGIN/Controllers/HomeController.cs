bddb266cf345c0e9ce72715017284fa2f924cda4
ViridianaEM <viridiana_em.uwu@hotmail.com>
2022-08-02 22:20:56 -0500

    first commit

---
+++ PRUEBAS_LOGIN/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using PRUEBAS_LOGIN.Permisos;
+
+namespace PRUEBAS_LOGIN.Controllers
+{
+
+    [ValidarSesion]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Men()
+        {
+            return View();
+        }
+        public ActionResult Women()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult Single_Product()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        public ActionResult CerrarSesion()
+        {
+            Session["usuario"] = null;
+            return RedirectToAction("Login", "Acceso");
+        }
+
+
+
+
+    }
+}
